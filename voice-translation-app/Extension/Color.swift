0f16e257be7ae20a7e454a163f9dbc6f97562e5c
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-04-16 22:25:40 +0300

    color extension

--- voice-translation-app/Extension/Color.swift
+++ voice-translation-app/Extension/Color.swift
@@ -1,8 +1,30 @@
-//
-//  Color.swift
-//  voice-translation-app
-//
-//  Created by Oleksandr Rolik on 10.04.2025.
-//
-
 import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let lightGreen = Color(hex: "#C9FFE0")
+    let white = Color(hex: "#F3F5F6")
+    let animalBackGroundGreen = Color(hex: "#ECFBC7")
+}
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+
+        let r = Double((rgb >> 16) & 0xFF) / 255
+        let g = Double((rgb >> 8) & 0xFF) / 255
+        let b = Double(rgb & 0xFF) / 255
+
+        self.init(red: r, green: g, blue: b)
+    }
+}
