6e080de081e494f10ae677e06c51b8bb4bb24c65
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-04-18 12:17:05 +0300

    GradientView

--- voice-translation-app/Controller/GradientView.swift
+++ voice-translation-app/Controller/GradientView.swift
@@ -1,8 +1,19 @@
-//
-//  GradientView.swift
-//  translator-swiftui
-//
-//  Created by Oleksandr Rolik on 18.04.2025.
-//
-
+import SwiftUI
 import Foundation
+
+struct GradientView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.theme.lightGreen, Color.theme.white]),
+                startPoint: .bottom,
+                endPoint: .top
+            )
+            .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    GradientView()
+}
